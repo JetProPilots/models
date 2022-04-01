d587b97a0f1b22045b954b4b9d0a733bc5f9a503
Nate Ross <nate.ross@alliedpayment.com>
2022-04-01 04:38:26 -0400

    12.0.11

--- models/crew-request.ts
+++ models/crew-request.ts
@@ -4,25 +4,32 @@ import { CrewRequest } from '../interfaces/crew-request'
 const schema = new Schema<CrewRequest>({
   legs: {
     required: true,
+    type: [],
   },
   client: {
     required: true,
+    type: Schema.Types.Mixed,
   },
   aircraft: {
     required: true,
+    type: Schema.Types.Mixed,
   },
   crew_needed: {
     required: true,
+    type: Boolean,
   },
   itinerary_international: {
     required: false,
     default: false,
+    type: Boolean,
   },
   additional_notes: {
     required: false,
+    type: String,
   },
   assigned: {
     required: false,
+    type: [],
   },
   revision: {
     required: true,
